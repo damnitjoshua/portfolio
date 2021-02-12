77e6e4b12360b99aeb041653c1b756f16ad7af81
Joshua Chew Jay Han <joshuachew8118@gmail.com>
2021-02-12 23:33:34 +0800

    Add files via upload

---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+
+export default function projects() {
+    return (
+        <div>
+            this page is not developed yet, this is just a placeholder for now.
+            maybe im just lazy who knows.
+        </div>
+    );
+}
