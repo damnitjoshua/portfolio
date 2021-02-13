520e960b6c9acc587632b7aa9b238b24725158ab
Joshua Chew Jay Han <joshuachew8118@gmail.com>
2021-02-13 21:24:54 +0800

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function projects() {
     return (
         <div>
-            this page is not developed yet, this is just a placeholder for now.
+            This page is not developed yet, just a placeholder for now.
             maybe im just lazy who knows.
         </div>
     );
